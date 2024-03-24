d340910464ebd1de78c9fbab1dbb1f8af431c352
Roshan Kandel <t3karo02@students.oamk.fi>
2024-03-24 19:46:39 +0200

    task2 completed with successful HTTP requests

--- todo/server/index.js
+++ todo/server/index.js
@@ -4,6 +4,7 @@ const { Pool}= require('pg');
 
 const app=express()
 app.use(cors())
+app.use(express.json())
 const port=3001
 
 app.get("/",(req,res)=>{
@@ -14,6 +15,20 @@ app.get("/",(req,res)=>{
             res.status(500).json({error:error.message})
         }
         res.status(200).json(result.rows)
+    })
+ })
+
+ app.post("/new",(req,res)=>{
+    const pool=openDb()
+
+    pool.query('insert into task (description) values ($1) returning *',
+    [req.body.description],
+    (error,result)=>{
+        if(error){
+            res.status(500).json({error:error.message})
+        }else{
+        res.status(200).json({id:result.rows[0].id})
+        }
     })
  })
 
